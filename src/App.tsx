4df7deab8b7788383701e135cbc50fbfd73ab8b8
sim-ware <samir.gossain@gmail.com>
2022-05-16 12:59:55 +0100

    Spotify logs in and out through desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+
+const spotifyLoginUrl = `${process.env.REACT_APP_AUTH_ENDPOINT}` + 
+    `?client_id=${process.env.REACT_APP_CLIENT_ID}` +
+    `&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}` +
+    `&response_type=${process.env.REACT_APP_RESPONSE_TYPE}`;
+
+function App() {  
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let localStorageToken = window.localStorage.getItem("token");
+
+    if (!localStorageToken && hash) {
+      localStorageToken = hash.substring(1)
+        .split("&")
+        .find(elem => elem.startsWith("access_token"))
+        ?.split("=")[1]!;
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", localStorageToken);
+    }
+
+    setToken(localStorageToken || "");
+  }, [])
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Concorder Host</code>
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        {
+          !token ? 
+          <a href={spotifyLoginUrl}>Login to Spotify</a>
+          : <button onClick={logout}>Logout</button>
+        }
       </header>
     </div>
   );
