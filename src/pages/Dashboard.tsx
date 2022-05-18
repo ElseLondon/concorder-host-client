7aa648ab3989d1723c0644b41f06a6b16d5b68c0
sim-ware <samir.gossain@gmail.com>
2022-05-18 11:29:52 +0100

    Uses latest React version 18.1.0

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 function Dashboard(props: any) { // fix any
-    console.log("Dashboard props", props);
-    return (
-        <div className="App">
-            <h2>I'm the dashboard.</h2>
-            <p>You love me &lt;3 </p>
-        </div>
-    );
+  
+  console.log("Dashboard props", props);
+
+  return (
+    <div className="App">
+      <h2>I'm the dashboard.</h2>
+      <p>You love me &lt;3 </p>
+    </div>
+  );
 }
 
 export default Dashboard;
