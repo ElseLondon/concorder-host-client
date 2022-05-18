f524becb33da203351b6ebf3ac5ecae3fe9ecf44
sim-ware <samir.gossain@gmail.com>
2022-05-18 09:24:08 +0100

    Changes pages from jsx to tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Dashboard(props) {
+function Dashboard(props: any) { // fix any
     console.log("Dashboard props", props);
     return (
         <div className="App">
---
+++ src/pages/Page-2.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page2() {
+  return (
+    <div>
+      <h2>Page 2</h2>
+      <p>I'm kinda boring :/</p>
+    </div>
+  );
+}
+
+export default Page2;
