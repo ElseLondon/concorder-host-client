5b9902f6314168d9a4328478f2d38b788c3f805e
sim-ware <samir.gossain@gmail.com>
2022-05-18 11:46:17 +0100

    Upgrades styled-components and react-dom packages to latest

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,17 +4,17 @@ import Sidebar from "./Sidebar";
 import Nav from "./Nav";
 
 function Layout(props: any) { // fix any as we have expected fields below
-    return (
-        <div>
-            <div style={{display: "flex"}}>
-                <Sidebar history={props.history}/>
-                <div style={{maxWidth: '800px'}}>
-                    {/* <Nav/> */}
-                    {props.children}
-                </div>
-            </div>
+  return (
+    <div>
+      <div style={{display: "flex"}}>
+        <Sidebar history={props.history}/>
+        <div style={{maxWidth: '800px'}}>
+          {/* <Nav/> */}
+          {props.children}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Layout;
