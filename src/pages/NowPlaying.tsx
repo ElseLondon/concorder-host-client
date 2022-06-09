7e643fd59135f2139475c9a6b11fc52bff525c6e
sim-ware <samir.gossain@gmail.com>
2022-06-09 17:04:37 +0100

    Adds pill button - needs correct colour and font

--- src/pages/NowPlaying.tsx
+++ src/pages/NowPlaying.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
 
+
 function NowPlaying() {
   return (
-    // create black content panel
-    // create header
-    // create body
     // create button
-    <div style={{padding: '24px'}}>
-      <h2>Now Playing</h2>
+    <div style={{
+        backgroundColor: '#000000', 
+        height: '27%', 
+        width: '258%', 
+        margin: '25px',
+        borderRadius: '10px',
+        padding: '340px 0',
+      }}
+    >
+      <h2 style={{color: 'white', textAlign: 'center'}}>No Tracks on The Queue</h2>
+      <p style={{color: 'white', textAlign: 'center'}}>
+        It seems like the play queue is empty at the moment. As soon as any 
+      </p>
+      <br/>
+      <p style={{color: 'white', textAlign: 'center'}}>
+        users connect, or you add songs to the Jukebox catalogue, tracks
+      </p>
+      <br/>
+      <p style={{color: 'white', textAlign: 'center'}}>
+        will show up here.
+      </p>
+      <br/>
+      <button 
+        style={{
+          margin: '0 auto', 
+          display: 'block',
+          backgroundColor: '#ddd',
+          border: 'none',
+          color: 'black',
+          padding: '10px 20px',
+          textAlign: 'center',
+          textDecoration: 'none',
+          cursor: 'pointer',
+          borderRadius: '16px',
+        }} 
+        type="button"
+        >
+          Manage Jukebox
+        </button>
     </div>
   );
 }
