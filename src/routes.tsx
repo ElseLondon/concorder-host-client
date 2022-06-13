e99c346bbb7c4aec9cc65ef336c264eeb8daea98
sim-ware <samir.gossain@gmail.com>
2022-06-13 15:28:18 +0100

    Installs MUI and uses Modal at root layer in routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -8,11 +8,53 @@ import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
 import LogOut from "./pages/LogOut";
 import Settings from "./pages/Settings";
-
+// ~modal time~ //
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+// // // // // //
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+// 
 
 export default function Routes() {
+  // 
+  const [open, setOpen] = React.useState(true);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  // 
+
   return (
     <BrowserRouter>
+      {/*  */}
+      <>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
+      </>
+      {/*  */}
       <Route render={(props)=>(
         <Layout {...props}>
           <Switch>
