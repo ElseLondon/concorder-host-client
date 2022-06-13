685e482c7e34503c6ff0bece360549f3ff523220
sim-ware <samir.gossain@gmail.com>
2022-06-13 11:31:44 +0100

    Fleshes out Jukebox page

--- src/pages/JukeBox.tsx
+++ src/pages/JukeBox.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from '../components/Header';
+import CopyText from '../components/CopyText';
+import PillButton from '../components/PillButton';
 
 
 export default function JukeBox() {
+  const headerText = "Jukebox is empty...";
+  const copyText = [
+    "You haven't added any tracks to the Jukebox catalogue yet. Add",
+    "some tracks below so that users can select tracks from the Jukebox.",
+  ];
+  const buttonText = "Add to Catalogue";
+
   return (
-    // create black content panel
-    // create header
-    // create body
-    // create button
-    <div style={{padding: '24px'}}>
-      <h2>JukeBox</h2>
-    </div>
+    <ContainerDiv>
+      <Header headerText={headerText}></Header>
+
+      <CopyText copyText={copyText}></CopyText>
+    
+      <PillButton buttonText={buttonText}></PillButton>
+    </ContainerDiv>
   );
 }
+
+const ContainerDiv = styled.div`
+  background-color: #000000; 
+  height: 20vh; 
+  width: 100vh; 
+  margin: 25px;
+  border-radius: 10px;
+  padding: 37vh 28vh 37vh 28vh;
+`;
